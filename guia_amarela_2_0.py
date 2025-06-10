3e34edb14b6b45bef7c29548740af8122fc78ca9
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-09 22:05:51 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -28,19 +28,7 @@ st.markdown("Selecione um lote no mapa ou filtre pela inscrição fiscal para vi
 # Mapa base
 m = folium.Map(location=[-25.46, -49.27], zoom_start=12, tiles='CartoDB positron')
 
-# Adiciona os lotes
-for idx, row in gdf.iterrows():
-    folium.GeoJson(
-        row["geometry"],
-        name=f"Lote {row['inscricao_fiscal']}",
-        tooltip=f"Inscrição Fiscal: {row['inscricao_fiscal']}",
-        popup=folium.Popup(f"""
-            <b>Inscrição Fiscal:</b> {row['inscricao_fiscal']}<br>
-            <b>Taxa de Ocupação:</b> {row['taxa_ocupacao']}<br>
-            <b>Coef. Aproveitamento:</b> {row['coeficiente_aproveitamento']}<br>
-            <b>Uso Permitido:</b> {row['uso_permitido']}
-        """, max_width=300)
-    ).add_to(m)
+
 
 # Exibe o mapa
 st_data = st_folium(m, width=1000, height=500)
