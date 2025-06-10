f7b9ff146a595becaa0445b8f1daf54eb81ea143
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-09 21:51:23 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -9,7 +9,7 @@ from shapely.geometry import Polygon
 shapefile_path = "lotes_curitiba.zip"
 
 # Carrega o GeoDataFrame a partir do zip
-gdf = gpd.read_file(f"zip://{os.path.abspath(shapefile_path)}")
+gdf = gpd.read_file(f"zip://{os.path.abspath(Trabalho-Final/lotes.zip)}")
 
 # Interface do Streamlit
 st.set_page_config(layout="wide")
