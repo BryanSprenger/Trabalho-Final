1a16f9943fdbb1bb034db16a04f829a7b3b0d680
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-11 16:39:04 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -18,7 +18,10 @@ st.set_page_config(**PAGE_CONFIG)
 st.markdown("Selecione um lote no mapa ou filtre pela inscrição fiscal para visualizar os dados urbanísticos.")
 
 # Mapa base
-m = folium.Map(location=[-25.46, -49.27], zoom_start=12, tiles='CartoDB positron')
+m = folium.Map(location=[-25.46, -49.27], zoom_start=12, tiles='CartoDB positron',
+                geo_data=polygons.to_json(), data=polygons).add_to(m)
+    folium.LayerControl().add_to(m)
+    folium_static(m)
 
 # Exibe o mapa
 st_data = st_folium(m, width=1000, height=500)
