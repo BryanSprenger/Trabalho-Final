ba89e46059c23de3fd7b01ba8f8be3e5f16a1a97
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-15 21:54:14 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -16,19 +16,8 @@ st.title("Guia Amarela Interativa")
 st.markdown("Selecione um lote no mapa ou filtre pela inscrição fiscal para visualizar os dados urbanísticos.")
 
 # --- Criação do Mapa Base Folium ---
-  def main():
-	st.title("Como adicionar mapas no StreamLit")
-	st.subheader("Baseado num caderno do Colab")
-	menu = ["Home","Mapa"]
-	choice = st.sidebar.selectbox('Menu',menu)
-	if choice == 'Home':
-		st.subheader("Página Inicial 1")
-	elif choice == 'Mapa':
-		st.subheader("Visualizar Mapa")
-		with st.echo():
-			m = folium.Map (location = [-25.5,-49.3], zoom_start =  9)
-			folium_static(m)
-	else: 
-		st.subheader("")
-if __name__ == '__main__':
-	main()
+m = folium.Map(location=[-25.46, -49.27], zoom_start=12, tiles="CartoDB positron")
+
+
+# --- Renderização do Mapa no Streamlit ---
+st_folium(m)
