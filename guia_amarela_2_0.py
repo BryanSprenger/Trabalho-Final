e2ae689707d809a93d04303983e5fe1648b00d68
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-15 21:45:49 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -1,7 +1,13 @@
 import streamlit as st
+from streamlit_folium import folium_static
 import folium
-from streamlit_folium import st_folium
 import geopandas as gpd
+import pandas as pd
+import plotly.express as px
+
+# Carregamento dos dados
+
+
 
 # --- Configuração da Página Streamlit ---
 st.set_page_config(page_title="Guia Amarela Interativa", page_icon=":scroll:", layout="centered")
@@ -10,11 +16,19 @@ st.title("Guia Amarela Interativa")
 st.markdown("Selecione um lote no mapa ou filtre pela inscrição fiscal para visualizar os dados urbanísticos.")
 
 # --- Criação do Mapa Base Folium ---
-m = folium.Map(location=[-25.46, -49.27], zoom_start=12, tiles="CartoDB positron")
+    m = folium.Map(location=[-25.5, -49.3], zoom_start=9)
+    folium.Choropleth(
+        geo_data=bairros_finais.to_json(),
+        name='estacionamentos por bairro',
+        data=bairros_finais,
+        columns=['OBJECTID', 'num_pto'],
+        key_on='feature.properties.OBJECTID',
+        fill_color='YlGn',
+        legend_name='Estacionamentos por bairro'
+    ).add_to(m)
+    folium.LayerControl().add_to(m)
+    folium_static(m)
 
-# --- Adição de Controle de Camadas ---
-folium.LayerControl().add_to(m)
-
-# --- Renderização do Mapa no Streamlit ---
-st_folium(m)
+if __name__ == '__main__':
+    main()
 
