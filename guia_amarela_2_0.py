a555587303a5adaa93fe331af97799b0e41dd648
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-15 22:08:39 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 # Carregamento dos dados
-url_lotes = "https://raw.githubusercontent.com/BryanSprenger/Trabalho-Final/main/lotes.geojson"
+url_lotes = "https://raw.githubusercontent.com/BryanSprenger/Trabalho-Final/main/lotes4.geojson"
 
 # Carrega o GeoDataFrame
 gdf = gpd.read_file(url_lotes)
