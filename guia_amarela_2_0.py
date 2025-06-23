460de5224ba34859a8305bb768756c3f1c28aece
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-23 20:03:28 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -362,6 +362,15 @@ elif pagina == "🏘️ Análise Estatística de Emissão de Alvarás":
                 "fillOpacity": 0.5
             }
         ).add_to(m_alvaras)
+        st.write(gdf_alvaras_lotes.head())
+        st.write("Tipos de geometria:", gdf_alvaras_lotes.geometry.geom_type.unique())
+        st.write("CRS do GeoDataFrame:", gdf_alvaras_lotes.crs)
+
+        lote_teste = gdf_alvaras_lotes.iloc[[0]]  # usa duplo colchete para manter como GeoDataFrame
+        folium.GeoJson(
+            lote_teste,
+            tooltip=folium.GeoJsonTooltip(fields=["INDFISCAL", "Uso(s) Alvará"])
+        ).add_to(m_alvaras)
     
         folium.LayerControl().add_to(m_alvaras)
         st_folium(m_alvaras, width=900, height=500)
