61a503b17db0a61a7322b8cf44a3ab0174ed33c7
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-06-30 09:56:56 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -124,7 +124,9 @@ if pagina == "🏠 Home":
     # Rodapé informal
     st.markdown("""
     <br>
-    <span style='font-size: 12px; color: gray;'>Este Projeto foi desenvolvido como trabalho final da disciplina de Desenvolvimento de Aplicações Geoespaciais da Pós-Graduação em Ciências Geodésicas da UFPR. Discente: Bryan Leonardo Franco Sprenger. Ano: 2025.</span>
+    <span style='font-size: 12px; color: gray;'>Este Projeto foi desenvolvido como trabalho final da disciplina de Desenvolvimento de Aplicações Geoespaciais da Pós-Graduação em Ciências Geodésicas da UFPR. 
+Discente: Bryan Leonardo Franco Sprenger. 
+Ano: 2025.</span>
     """, unsafe_allow_html=True)
 
 # ---------------------------------------------------------------------------- POTENCIAL CONSTRUTIVO ------------------------------------------------------------------------------------
