babdede17fec38739f87235a2eb0caab24abfaa8
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-07-03 13:24:09 -0300

    Update guia_amarela_2_0.py

--- guia_amarela_2_0.py
+++ guia_amarela_2_0.py
@@ -264,6 +264,16 @@ elif pagina == "📐 Área de Ocupação":
 
     ind_fiscal_2 = st.session_state.get("indfiscal_global", "").strip().upper()
 
+    # Verifica se há alvará correspondente
+    if 'df_alvaras' in globals():
+        df_alvaras['INDFISCAL'] = df_alvaras['INDFISCAL'].astype(str).str.replace('.', '', regex=False).str.zfill(8)
+        alvaras_encontrados = df_alvaras[df_alvaras['INDFISCAL'] == ind_fiscal_2]
+    
+        if not alvaras_encontrados.empty:
+            st.success(f"✅ {len(alvaras_encontrados)} alvará(s) encontrado(s) para a IF {ind_fiscal_2}.")
+        else:
+            st.info(f"ℹ️ Nenhum alvará encontrado para a IF {ind_fiscal_2}.")
+
     if ind_fiscal_2:
         gdf_lotes['INDFISCAL'] = gdf_lotes['INDFISCAL'].astype(str)
         ind_fiscal_2 = ind_fiscal_2.strip()
